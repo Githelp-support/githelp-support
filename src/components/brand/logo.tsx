8e2f4d34a734d43313f3ad432f0ad0e20e21b488
Stian <stian@Stian.localdomain>
2026-03-24 12:20:12 +0100

    feat: Decrease the size of the logo shown in the upper left corner from 32x32 pixel...
    Task: 1abe7e46
    Source: slack
    Requested by: U04E9DZ5ELF

--- src/components/brand/logo.tsx
+++ src/components/brand/logo.tsx
@@ -18,8 +18,8 @@ export function Logo({ className, href, variant = "light" }: LogoProps) {
 
   const svg = (
     <svg
-      width="32"
-      height="32"
+      width="25"
+      height="25"
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
